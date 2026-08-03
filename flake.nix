5751db231370f45eb7f30d3e6f94ba81dfc84bec
Håkon Anders Strømsodd <haakon8855@gmail.com>
2026-08-03 14:13:21 +0200

    Add flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "NixPad system config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05"; # or nixos-25.11 etc, match what you're on
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations.NixPad = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
