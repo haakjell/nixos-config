0418c4893a2579c18affd962a30b001329c2da18
Håkon Anders Strømsodd <haakon8855@gmail.com>
2026-08-03 15:27:01 +0200

    Add weekly garbage collection

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,12 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 7d";
+  };
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
